d7e83b87c43edee9f31fc07429ccef0f6f2469bb
Michael Lin <60127987+michaelwlin@users.noreply.github.com>
2021-02-08 17:43:12 -0800

    Delete secret.js

--- server/secret.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host : 'bloom-dev.cs4gquu5vprb.us-east-2.rds.amazonaws.com',
-    user : 'bloom_engineer',
-    password : 'feynman88diagram%@',
-    database : 'bloom_dev',
-}
-
