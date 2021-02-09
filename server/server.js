ef96ffc46be58e032397587890817637236164d6
Michael Lin <60127987+michaelwlin@users.noreply.github.com>
2021-02-08 17:50:25 -0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 const mysql = require('mysql')
 const app = express();
 const bodyParser = require('body-parser');
-const secret = require('./secret')
+const secret = require('./secret');
 
 app.use(cors());
 app.use(express.json());
